b92952dff5dc4cc258dd24e42d47699a0c8b2f18
Kiana Toossi <Kiana@MacBook-Pro-8.wireless.newschool.edu>
2018-05-04 09:55:26 -0400

    arrows

--- studio/assets/js/mad-men.js
+++ studio/assets/js/mad-men.js
@@ -1,5 +1,3 @@
-// MAD MEN
-
 var bodyHeight = document.documentElement.offsetHeight
 
 var items = document.querySelectorAll('.item')
@@ -54,52 +52,3 @@ window.addEventListener( 'scroll', function() {
 Number.prototype.map = function (in_min, in_max, out_min, out_max) {
   return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
-
-
-
-// var outer = document.querySelector('.outer')
-// var image = document.querySelector('.image')
-//
-// outer.addEventListener('scroll', function(event) {
-// 	console.log(outer.scrollTop)
-// 	image.style.transform = 'scale(' + outer.scrollTop / 10 + ')'
-// })
-
-/////////////////
-
-// function openModal() {
-//   document.getElementById('myModal').style.display = "block";
-// }
-//
-// function closeModal() {
-//   document.getElementById('myModal').style.display = "none";
-// }
-//
-// var slideIndex = 1;
-// showSlides(slideIndex);
-//
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-//
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-//
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("demo");
-//   var captionText = document.getElementById("caption");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-//   captionText.innerHTML = dots[slideIndex-1].alt;
-// }
